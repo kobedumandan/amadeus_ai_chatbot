46964089f7768ab6eecd3bdaa6a2238a6a10c91b
kobedumandan <kgdumandan@gmail.com>
2026-06-29 14:03:57 +0800

    added: partial send message functionality.

--- amadeus_ai_chatbot/src/App.jsx
+++ amadeus_ai_chatbot/src/App.jsx
@@ -5,27 +5,25 @@ import SideBar from "./components/SideBar";
 import ContentPane from "./components/ContentPane";
 import "./App.css";
 
-var CHAT_MESSAGES = [
-  { role: "sender", message: "Hello amadeus!" },
-  { role: "reciever", message: "Hello kobe!" },
-];
-
 function App() {
-  const [count, setCount] = useState(0);
+  const [messages, setMessages] = useState([
+    { role: "sender", message: "Hello amadeus!" },
+    { role: "reciever", message: "Hello kobe!" },
+  ])
 
   return (
     <div className="app-body">
       <SideBar title={"@"} />
 
       <ContentPane header={<PageHeader title={"amadeus"}></PageHeader>}>
-        {CHAT_MESSAGES.map((msg, index) => (
+        {messages.map((msg, index) => (
           <div className={`message_row ${msg.role}`}>
             <div className="chat_messages" key={index}>
               {msg.message}
             </div>
           </div>
         ))}
-        <InputBar />
+        <InputBar setMessages={setMessages}/>
       </ContentPane>
     </div>
   );
--- amadeus_ai_chatbot/src/components/InputBar.jsx
+++ amadeus_ai_chatbot/src/components/InputBar.jsx
@@ -1,13 +1,38 @@
-import '../styles/InputBar.css';
-import { SendInput } from '../assets/svg/svg.jsx';
+import "../styles/InputBar.css";
+import { SendInput } from "../assets/svg/svg.jsx";
 
-export default function InputBar() {
-    return (
-        <div className='searchbar_wrapper'>
-            <input className='search_input' type="text" size={70} placeholder='Ask me Anything!'/>
-            <button className='input-bar-btn'>
-                <SendInput className="input_btn"/>
-            </button>
-        </div>
-    );
+export default function InputBar({ setMessages }) {
+
+  function SendMessage() {
+    const msg = document.getElementById("search-bar-input");
+    setMessages((prev) => [
+      ...prev,
+      {
+        role: "sender",
+        message: msg.value,
+      },
+    ]);
+
+    clearInputBar()
+  }
+
+  function clearInputBar() {
+    const msg = document.getElementById("search-bar-input");
+    msg.value = ""
+  }
+
+  return (
+    <div className="searchbar_wrapper">
+      <input
+        id="search-bar-input"
+        className="search_input"
+        type="text"
+        size={65}
+        placeholder="Ask me Anything!"
+      />
+      <button className="input-bar-btn" onClick={SendMessage}>
+        <SendInput className="input_btn" />
+      </button>
+    </div>
+  );
 }
