89b7b2cb4ec310be4552ab4ecd22af4bce583547
kobedumandan <kgdumandan@gmail.com>
2026-06-30 08:52:19 +0800

    added: connected to openrouter API, just create an .env file and put api key on REACT_APP_OPENROUTER_API_KEY

--- amadeus_ai_chatbot/src/components/InputBar.jsx
+++ amadeus_ai_chatbot/src/components/InputBar.jsx
@@ -2,8 +2,7 @@ import "../styles/InputBar.css";
 import { SendInput } from "../assets/svg/svg.jsx";
 
 export default function InputBar({ setMessages }) {
-
-  function SendMessage() {
+  async function SendMessage() {
     const msg = document.getElementById("search-bar-input");
     setMessages((prev) => [
       ...prev,
@@ -13,12 +12,56 @@ export default function InputBar({ setMessages }) {
       },
     ]);
 
-    clearInputBar()
+    const userMsg = msg.value;
+
+    clearInputBar();
+
+    const resp = await fetch("https://openrouter.ai/api/v1/chat/completions", {
+      method: "POST",
+      headers: {
+        Authorization:
+          `Bearer ${import.meta.env.VITE_OPENROUTER_API_KEY}`,
+        // "HTTP-Referer": "<YOUR_SITE_URL>", // Optional. Site URL for rankings on openrouter.ai.
+        // "X-OpenRouter-Title": "<YOUR_SITE_NAME>", // Optional. Site title for rankings on openrouter.ai.
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        model: "openrouter/free",
+        messages: [
+          {
+            role: "user",
+            content: userMsg,
+          },
+        ],
+      }),
+      max_tokens: 256,
+    });
+
+    if (!resp.ok) {
+      setMessages((prev) => [
+        ...prev,
+        {
+          role: "system",
+          message: data.error.message,
+        },
+      ]);
+      return;
+    }
+
+    const data = await resp.json();
+
+    setMessages((prev) => [
+      ...prev,
+      {
+        role: "receiver",
+        message: data.choices[0].message.content,
+      },
+    ]);
   }
 
   function clearInputBar() {
     const msg = document.getElementById("search-bar-input");
-    msg.value = ""
+    msg.value = "";
   }
 
   return (
